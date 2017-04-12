945864a1512f54bc8e9c1dd4d65fdc44f2e0937d
april418 <apri.4.18@gmail.com>
2017-04-12 17:28:27 +0900

    itamae使ってないの忘れてた

--- etc/spec/spec_helper.rb
+++ etc/spec/spec_helper.rb
@@ -1,15 +1,15 @@
 require 'serverspec'
-require 'yaml'
-require "itamae/node"
+#require 'yaml'
+#require "itamae/node"
 
 set :backend, :exec
 
 USER = ENV['USER']
 HOME = ENV['HOME']
 
-def node
-  return @node if @node
-  yaml = YAML.load_file("#{Dir.pwd}/node.yml")
-  @node = Itamae::Node.new(yaml, Specinfra.backend)
-end
+#def node
+#  return @node if @node
+#  yaml = YAML.load_file("#{Dir.pwd}/node.yml")
+#  @node = Itamae::Node.new(yaml, Specinfra.backend)
+#end
 
