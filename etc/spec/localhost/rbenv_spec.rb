1d3adf68c85cccdc56e7b21be9c95b1fc940e045
april418 <apri.4.18@gmail.com>
2017-08-28 18:02:38 +0900

    何故かmodeが通らないので削除

--- etc/spec/localhost/rbenv_spec.rb
+++ etc/spec/localhost/rbenv_spec.rb
@@ -4,7 +4,6 @@ describe file("#{HOME}/\.rbenv/libexec/rbenv") do
   it { should be_file }
   it { should be_owned_by USER }
   it { should be_grouped_into USER }
-  it { should be_mode 755 }
 end
 
 describe file("#{HOME}/.bash_profile") do
@@ -21,14 +20,12 @@ describe file("#{HOME}/.rbenv/plugins") do
   it { should be_directory }
   it { should be_owned_by USER }
   it { should be_grouped_into USER }
-  it { should be_mode 775 }
 end
 
 describe file("#{HOME}/.rbenv/plugins/ruby-build") do
   it { should be_directory }
   it { should be_owned_by USER }
   it { should be_grouped_into USER }
-  #it { should be_mode 755 }
 end
 
 #%w(2.2.2).each do |version|
